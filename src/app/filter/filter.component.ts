78531de307f6cc03b257817bc4ec76e1970bb9c0
DevWurm <devwurm@devwurm.net>
2016-08-09 17:41:42 +0200

    Removing date-selector component

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {ArticleSelectorComponent} from "./article-selector/article-selector.component";
-import {DateSelectorComponent} from "./date-selector/date-selector.component";
 
 @Component({
   selector: 'pc-filter',
   templateUrl: 'filter.component.html',
   styleUrls: ['filter.component.css'],
-  directives: [ArticleSelectorComponent, DateSelectorComponent]
+  directives: [ArticleSelectorComponent]
 })
 export class FilterComponent implements OnInit {
 
