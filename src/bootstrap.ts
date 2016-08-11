94e6896badd2d34c5d4c21cdc7d3de1400874538
DevWurm <devwurm@devwurm.net>
2016-08-11 17:05:17 +0200

    Adding Http providers to boostrapping step

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,9 +2,10 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import {appRouterProviders} from "./app/app.routes";
+import {HTTP_PROVIDERS} from "@angular/http";
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [appRouterProviders]);
+bootstrap(AppComponent, [appRouterProviders, HTTP_PROVIDERS]);
