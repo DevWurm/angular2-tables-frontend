5277268f94aeaefed591d9e7a02e990115fcc7db
DevWurm <devwurm@devwurm.net>
2016-08-16 14:36:37 +0200

    Connection Article selection from ArticelList to global selection state

--- src/app/filter/article-selector/article-selector.component.ts
+++ src/app/filter/article-selector/article-selector.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
 import {PolymerElement} from "@vaadin/angular2-polymer";
 import {AvailableArticlesService} from "./shared/available-articles.service";
+import {ArticleRange} from "../../shared/article-selection/article-range";
+import {ArticleSelection} from "../../shared/article-selection/article-selection";
+import {SelectionMode} from "../../shared/article-selection/selection-mode.enum";
+import {ArticleSelectionService} from "../../shared/article-selection/article-selection.service";
 require('!include-loader!../../../../bower_components/vaadin-grid/vaadin-grid.html');
 
 @Component({
@@ -22,8 +26,7 @@ export class ArticleSelectorComponent implements OnInit, AfterViewInit {
   private gridSize: number = 10;
   private gridData = this.getArticles.bind(this);
 
-  constructor(private articlesService: AvailableArticlesService) {
-  }
+  constructor(private articlesService: AvailableArticlesService, private selectionService: ArticleSelectionService) {}
 
   ngOnInit() {
   }
@@ -51,11 +54,59 @@ export class ArticleSelectorComponent implements OnInit, AfterViewInit {
     );
   }
 
-  updateSelection(selection) {
+  updateSelection(selection, getItem) {
     if (selection.mode == 'all') {
-      console.log(selection.deselected());
+      this.generateArticleRanges(selection.deselected(), getItem).then(articleRanges => {
+        this.selectionService.articleSelection = new ArticleSelection(articleRanges, SelectionMode.EXCLUDING);
+      });
     } else {
-      console.log(selection.selected());
+      this.generateArticleRanges(selection.selected(), getItem).then(articleRanges => {
+        this.selectionService.articleSelection = new ArticleSelection(articleRanges, SelectionMode.INCLUDING);
+      })
     }
+  }
+
+  generateArticleRanges(selections: Array<number>, getItem: Function) {
+    let rangePrmss =  selections.sort()
+      .reduce((acc: Array<Array<number>>, curr: number) => {
+        if (acc.length == 0) {
+          return [[curr]];
+        } else {
+          let initGroups = acc.slice(0, acc.length - 1);
+          let lastGroup = acc[acc.length - 1];
+          let lastElement = lastGroup[lastGroup.length -1];
+
+          if (lastElement == (curr - 1)) {
+            return initGroups.concat([lastGroup.concat(curr)]);
+          } else {
+            return initGroups.concat([lastGroup, [curr]]);
+          }
+        }
+      }, [])
+      .map(rangeIndices => {
+        return {beginning: rangeIndices[0], end: rangeIndices[rangeIndices.length -1]};
+      })
+      .map(rangeBordersIndices => {
+        let beginningArticlePrms = new Promise<string>((resolve, reject) => {
+          getItem(rangeBordersIndices.beginning, (err, item) => {
+            return err ? reject(err) : resolve(item.article);
+          }, false);
+        });
+
+        let endArticlePrms = new Promise<string>((resolve, reject) => {
+          getItem(rangeBordersIndices.end, (err, item) => {
+            return err ? reject(err) : resolve(item.article);
+          }, false);
+        });
+
+        return Promise.all([beginningArticlePrms, endArticlePrms]);
+      })
+      .map(rangePrms => {
+        return rangePrms.then(([beginning, end]) => {
+          return new ArticleRange(beginning, end);
+        })
+      });
+
+    return Promise.all(rangePrmss);
   }
 }
