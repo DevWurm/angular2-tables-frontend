e6358138c5c6824638638c6f59c4fe14fc6340f1
DevWurm <devwurm@devwurm.net>
2016-08-16 14:11:48 +0200

    Fixing incorrect path in src/app/shared/index.ts

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,4 +2,4 @@ export * from "./api-configuration";
 export * from "./config-file-api-configuration.service";
 export * from './article-selection/article-range';
 export * from './article-selection/article-selection';
-export * from './article-selection/selection-mode';
+export * from './article-selection/selection-mode.enum';
