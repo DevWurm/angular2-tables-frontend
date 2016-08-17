c215e3a0df4b9d4bf9eaa0c443985c01f3886b85
DevWurm <devwurm@devwurm.net>
2016-08-17 14:45:12 +0200

    Adapting AvailiableArticle service to new ApiConfiguration interface

--- src/app/filter/article-selector/shared/available-articles.service.ts
+++ src/app/filter/article-selector/shared/available-articles.service.ts
@@ -5,18 +5,14 @@ import {ApiConfiguration, API_CONFIGURATION_TOKEN} from "../../../shared";
 @Injectable()
 export class AvailableArticlesService {
 
-  private apiAddr: String;
-
-  constructor(private http: Http, @Inject(API_CONFIGURATION_TOKEN) private apiConfig: ApiConfiguration) {
-    this.apiAddr = `http://${this.apiConfig.apiUrl ? this.apiConfig.apiUrl : ""}${this.apiConfig.apiPort ? ":" + this.apiConfig.apiPort: ""}/api/v${this.apiConfig.apiVersion}`
-  }
+  constructor(private http: Http, @Inject(API_CONFIGURATION_TOKEN) private apiConfig: ApiConfiguration) {}
 
   getAllArticles() {
-    return this.http.get(`${this.apiAddr}/articles`).map(res => res.json());
+    return this.http.get(`${this.apiConfig.apiBaseAddr}/articles`).map(res => res.json());
   }
 
   getArticles(index: number, count: number) {
-    return this.http.get(`${this.apiAddr}/articles?index=${index}&count=${count}`).map(res => res.json());
+    return this.http.get(`${this.apiConfig.apiBaseAddr}/articles?index=${index}&count=${count}`).map(res => res.json());
   }
 
 }
