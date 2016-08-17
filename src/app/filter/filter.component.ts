8e15131910bec37c80ef5857d51cdf19cfbae778
DevWurm <devwurm@devwurm.net>
2016-08-17 20:15:37 +0200

    Adding DatesSelector to filter component

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {ArticleSelectorComponent} from "./article-selector/article-selector.component";
+import {DatesSelectorComponent} from "./date-selector/dates-selector.component";
 
 @Component({
   selector: 'pc-filter',
   templateUrl: 'filter.component.html',
   styleUrls: ['filter.component.css'],
-  directives: [ArticleSelectorComponent]
+  directives: [ArticleSelectorComponent, DatesSelectorComponent]
 })
 export class FilterComponent implements OnInit {
 
