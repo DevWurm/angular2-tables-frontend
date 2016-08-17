d8c92af80025ae9cedf0371153c76356a245ef07
DevWurm <devwurm@devwurm.net>
2016-08-17 17:35:47 +0200

    Moving counts service into seperate subdirectory

--- src/app/counts/counts-list/shared/counts/counts.service.ts
+++ src/app/counts/counts-list/shared/counts/counts.service.ts
@@ -1,8 +1,8 @@
 import {Injectable, Inject} from '@angular/core';
 import {Http} from "@angular/http";
-import {ApiConfiguration, API_CONFIGURATION_TOKEN} from "../../../shared";
-import {ArticleSelectionService} from "../../../shared/article-selection/article-selection.service";
-import {SelectionMode} from "../../../shared/article-selection/selection-mode.enum";
+import {ApiConfiguration, API_CONFIGURATION_TOKEN} from "../../../../shared";
+import {ArticleSelectionService} from "../../../../shared/article-selection/article-selection.service";
+import {SelectionMode} from "../../../../shared/article-selection/selection-mode.enum";
 
 @Injectable()
 export class CountsService {
