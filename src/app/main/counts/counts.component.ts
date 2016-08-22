19dbf40dea0d2dfd9d0d1724897ee60aeab4a0f7
DevWurm <devwurm@devwurm.net>
2016-08-22 18:07:50 +0200

    Styling counts view

--- src/app/main/counts/counts.component.ts
+++ src/app/main/counts/counts.component.ts
@@ -1,12 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {CountsListComponent} from "./counts-list/counts-list.component";
+import {PolymerElement} from "@vaadin/angular2-polymer";
+require("!include-loader!../../../../bower_components/paper-button/paper-button.html")
 
 @Component({
   selector: 'app-counts',
   templateUrl: 'counts.component.html',
   styleUrls: ['counts.component.css'],
-  directives: [CountsListComponent]
+  directives: [
+    CountsListComponent,
+    PolymerElement('paper-button')
+  ]
 })
 export class CountsComponent implements OnInit {
   private title = "Pagecounts";
