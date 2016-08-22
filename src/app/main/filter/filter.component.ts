94078e22ef6abe33beee587771d7d0c84f2be6ad
DevWurm <devwurm@devwurm.net>
2016-08-22 16:41:14 +0200

    Styling filter view

--- src/app/main/filter/filter.component.ts
+++ src/app/main/filter/filter.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {ArticleSelectorComponent} from "./article-selector/article-selector.component";
 import {DatesSelectorComponent} from "./date-selector/dates-selector.component";
+import {PolymerElement} from "@vaadin/angular2-polymer";
+require("!include-loader!../../../../bower_components/paper-button/paper-button.html")
 
 @Component({
   selector: 'pc-filter',
   templateUrl: 'filter.component.html',
   styleUrls: ['filter.component.css'],
-  directives: [ArticleSelectorComponent, DatesSelectorComponent]
+  directives: [
+    ArticleSelectorComponent,
+    DatesSelectorComponent,
+    PolymerElement('paper-button')
+  ]
 })
 export class FilterComponent implements OnInit {
 
