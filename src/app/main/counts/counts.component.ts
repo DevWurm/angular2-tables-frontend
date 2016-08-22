505224f65acad2f88f2c1208fbbf8e63d840ef71
DevWurm <devwurm@devwurm.net>
2016-08-22 18:28:30 +0200

    Adding tab view to counts view to make different grids possible

--- src/app/main/counts/counts.component.ts
+++ src/app/main/counts/counts.component.ts
@@ -2,7 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {CountsListComponent} from "./counts-list/counts-list.component";
 import {PolymerElement} from "@vaadin/angular2-polymer";
-require("!include-loader!../../../../bower_components/paper-button/paper-button.html")
+require("!include-loader!../../../../bower_components/paper-button/paper-button.html");
+require("!include-loader!../../../../bower_components/paper-tabs/paper-tabs.html");
+require("!include-loader!../../../../bower_components/paper-tabs/paper-tab.html");
+require("!include-loader!../../../../bower_components/iron-pages/iron-pages.html");
 
 @Component({
   selector: 'app-counts',
@@ -10,11 +13,15 @@ require("!include-loader!../../../../bower_components/paper-button/paper-button.
   styleUrls: ['counts.component.css'],
   directives: [
     CountsListComponent,
-    PolymerElement('paper-button')
+    PolymerElement('paper-button'),
+    PolymerElement('paper-tabs'),
+    PolymerElement('paper-tab'),
+    PolymerElement('iron-pages')
   ]
 })
 export class CountsComponent implements OnInit {
   private title = "Pagecounts";
+  private selected: number = 0;
 
   constructor(private router: Router) {
   }
