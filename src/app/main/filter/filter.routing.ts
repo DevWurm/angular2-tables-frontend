ed37ac328d0e8e304db6f8bae6f9a4b5cd84ce9a
DevWurm <devwurm@devwurm.net>
2016-09-06 12:59:42 +0200

    Updating to Angular2.rc6

---
+++ src/app/main/filter/filter.routing.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+import {FilterComponent} from "./filter.component";
+
+export const filterRoutes: Routes = [
+  { path: 'filter', component: FilterComponent}
+];
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,4 +1,12 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {AppModule} from "./app/app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+/* TODO: Remove
+
+
+
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import {appRouterProviders} from "./app/app.routes";
@@ -10,4 +18,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [appRouterProviders, HTTP_PROVIDERS, {provide: API_CONFIGURATION_TOKEN, useClass: ConfigFileApiConfigurationService}]);
+bootstrap(AppComponent, [appRouterProviders, HTTP_PROVIDERS, ]);
+*/
