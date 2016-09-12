4b26c6ff77abe52d12b01a224cca94b2406bd8c3
DevWurm <devwurm@devwurm.net>
2016-09-12 14:31:55 +0200

    Adding PrimeNg DataTable setup

--- src/app/main/articles/results/results-list-primeng/results-list-primeng.component.ts
+++ src/app/main/articles/results/results-list-primeng/results-list-primeng.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit, OnDestroy} from '@angular/core';
+import {Subscription} from "rxjs";
+import {ArticlesService} from "../../shared/articles-service/articles.service";
+import {FilterService} from "../../shared/filter-service/filter.service";
+import {SortingSelection} from "../../shared/filter/sorting-selection/sorting-selection";
+import {Sorting} from "../../shared/filter/sorting-selection/sorting";
+import {SortingOrder} from "../../shared/sorting/sorting-order.enum";
 
 @Component({
   selector: 'pc-results-list-primeng',
   templateUrl: 'results-list-primeng.component.html',
   styleUrls: ['results-list-primeng.component.css']
 })
-export class ResultsListPrimengComponent implements OnInit {
+export class ResultsListPrimengComponent implements OnInit, AfterViewInit, OnDestroy {
+  private subscriptions: Array<Subscription> = [];
 
-  constructor() { }
+  private articlesData: Array<any> = [];
+  private rows: number = 15;
+  private gridSize: number = 3 * this.rows;
+
+  private columns: Array<any> = [{field: 'article', header: "Article", sortable: true}];
+
+  constructor(private articlesService: ArticlesService, private filterService: FilterService) { }
 
   ngOnInit() {
+    this.setupColumns();
   }
 
+  ngAfterViewInit() {}
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    })
+  }
+
+  getArticles(params) {
+    const sortingSelection = new SortingSelection(params.sortField ? [new Sorting(params.sortField, params.sortOrder == -1 ? SortingOrder.DESC : SortingOrder.ASC)] : []);
+    this.subscriptions.push(this.articlesService.getArticles(params.first, params.rows, sortingSelection, this.filterService.filter).subscribe(
+      data => {
+        if ((this.gridSize <= params.first + params.rows) && !(data.length < params.rows)) {
+          this.gridSize += this.rows;
+        } else if ((data.length < params.rows)) {
+          this.gridSize = params.first + data.length;
+        }
+
+        this.articlesData = data
+      },
+      error => console.error(error)
+    ));
+  }
+
+  setupColumns() {
+    this.filterService.filter.datesFilter.forEach(date => {
+      this.columns = this.columns.concat({field: date, header: date, sortable: true});
+    });
+  }
 }
--- src/app/main/articles/results/results-list-primeng/results-list-primeng.module.ts
+++ src/app/main/articles/results/results-list-primeng/results-list-primeng.module.ts
@@ -2,8 +2,11 @@ import {NgModule}      from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {DataTableModule} from "primeng/components/datatable/datatable";
 import {SharedModule} from "primeng/components/common/shared";
-import "!include-loader?script!primeui/primeui-ng-all.min.js";
 import {ResultsListPrimengComponent} from "./results-list-primeng.component";
+import "!include-loader?script!primeui/primeui-ng-all.min.js";
+import "!include-loader?style!primeng/resources/primeng.min.css";
+import "!include-loader?style!primeng/resources/themes/delta/theme.css";
+import "!include-loader?style!font-awesome/css/font-awesome.min.css";
 
 @NgModule({
   imports: [CommonModule, DataTableModule, SharedModule],
